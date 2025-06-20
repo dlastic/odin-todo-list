1f319af867a4c3c5786a98bb05f88a954e5a9c67
dlastic <daniellastic@gmail.com>
2025-06-20 15:49:37 +0200

    Implement event binding in navController.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import "./styles.css";
 import { addTodo } from "./modules/todoManager";
 import { renderTodos, renderView } from "./modules/domController";
+import bindEvents from "./modules/navController";
 
-addTodo("Study JavaScript", "Work on todo app", "2025-06-20", "High");
-addTodo("Read book", "Finish current chapter", "2025-06-19", "Medium");
-addTodo("Study JavaScript", "Work on todo app", "2025-06-20", "High");
-addTodo("Read book", "Finish current chapter", "2025-06-19", "Medium");
-addTodo("Study JavaScript", "Work on todo app", "2025-06-20", "High");
-addTodo("Read book", "Finish current chapter", "2025-06-19", "Medium");
-addTodo("Study JavaScript", "Work on todo app", "2025-06-20", "High");
-addTodo("Read book", "Finish current chapter", "2025-06-19", "Medium");
+// addTodo("Study JavaScript", "Work on todo app", "2025-06-20", "High");
+// addTodo("Read book", "Finish current chapter", "2025-06-19", "Medium");
 
-renderView();
+renderView("my-day");
+bindEvents();
--- src/modules/navController.js
+++ src/modules/navController.js
@@ -1,5 +1,11 @@
+import { renderView } from "./domController";
 
+export default function bindEvents() {
+  const homeBtns = document.querySelectorAll(".home-section button");
 
-function bindEvents() {
-  
+  homeBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      renderView(btn.dataset.view);
+    });
+  });
 }
